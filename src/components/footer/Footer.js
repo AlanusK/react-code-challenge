31cba7a353b2d190ecf396d45f66134098dcc0d0
AlanusK <akajara@gmail.com>
2022-04-10 02:45:30 +0300

    feat: improve footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='flex flex-col fixed bottom-0 left-0 right-0 bg-neutral-800'>
-            <div className='flex flex-col justify-center h-24 px-20 top-0 left-0 right-0'>
+        <div className='flex flex-col fixed bottom-0 left-0 right-0 bg-neutral-800 h-48'>
+            <div className='flex flex-col justify-center h-1/2 px-20 top-0 left-0 right-0'>
                 <h1 className='text-white text-sm py-3'>Home | Terms and Conditions | Privacy Policy | Collection Statement | Help | Manage Account</h1>
                 <h1 className='text-white text-sm'>Copyright © 2016 DEMO Streaming. All Rights Reserved</h1>
             </div>
-            <div className='flex items-center justify-between h-24 px-20 pr-32'>
+            <div className='flex items-center justify-between h-1/2 px-20 pr-32'>
                 <div className='flex'>
                     <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 160 320"><path fill="#FFF" d="M166.1 52h-47.4c-5.6 0-11.9 7.4-11.9 17.2v34.3h59.3v48.8h-59.3V299h-56V152.3H0v-48.8h50.8V74.7C50.8 33.5 79.4 0 118.6 0H166v52z" /></svg>
                     <svg xmlns="http://www.w3.org/2000/svg" height="30" width="80" viewBox="0 0 368.40001 299.39999"><path fill="#FFF" d="M368.4 35.5c-13.6 6-28.1 10.1-43.4 11.9 15.6-9.4 27.6-24.2 33.2-41.8-14.6 8.7-30.8 15-48 18.3C296.4 9.2 276.8 0 255 0c-41.7 0-75.6 33.8-75.6 75.6 0 5.9.7 11.7 2 17.2-62.8-3.1-118.5-33.2-155.8-79C19.1 25 15.4 38 15.4 51.8c0 26.2 13.3 49.4 33.6 62.9-12.4-.4-24-3.8-34.2-9.4v.9c0 36.6 26.1 67.2 60.6 74.1-6.3 1.7-13 2.7-19.9 2.7-4.9 0-9.6-.5-14.2-1.4 9.6 30 37.5 51.9 70.6 52.5C86 254.4 53.4 266.5 18 266.5c-6.1 0-12.1-.4-18-1.1 33.4 21.5 73.2 34 115.9 34 139 0 215.1-115.2 215.1-215.1 0-3.3-.1-6.5-.2-9.8 14.6-10.5 27.4-23.8 37.6-39z" /></svg>
