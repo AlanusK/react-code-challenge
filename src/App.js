c168f61cd153890fed54d244953a8f5a8ec0256b
AlanusK <akajara@gmail.com>
2022-04-10 05:14:00 +0300

    feat: improve home page to link with other pagees

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import Footer from './components/footer';
+import Header from './components/header';
 import Routes from './Routes';
 
 function App() {
   return (
     <div className='h-full flex flex-col'>
-      <div className=''>
+      <Header/>
+      <div className='pt-16'>
         <Routes />
       </div>
+      <Footer />
     </div>
   );
 }
